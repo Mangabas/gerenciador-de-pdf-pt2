52646453c3b4b8f45b05af692f2eca1b000ce42d
Lucas Mangabeira <144847349+Mangabas@users.noreply.github.com>
2025-06-14 17:50:27 -0300

    Update ArquivoPDF.java

--- src/models/ArquivoPDF.java
+++ src/models/ArquivoPDF.java
@@ -4,10 +4,7 @@ import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.Serializable;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.nio.file.StandardCopyOption;
+import java.nio.file.*;
 import java.util.ArrayList;
 
 public abstract class ArquivoPDF implements Serializable {
